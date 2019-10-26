3c2122f82b82bfdb4fdb24215aeb1affb28743f5
fyeung21 <fyeung21@gmail.com>
2019-10-26 16:02:24 -0700

    add yarn install

---
+++ server/public/precache-manifest.3a36f84c60878911c5807db4c32b4aed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1dbf60c6d03a9ac24ba5",
+    "url": "/static/js/main.1995e2e0.chunk.js"
+  },
+  {
+    "revision": "3a1f8782792caf084163",
+    "url": "/static/js/2.34a8ba66.chunk.js"
+  },
+  {
+    "revision": "1dbf60c6d03a9ac24ba5",
+    "url": "/static/css/main.e0e73651.chunk.css"
+  },
+  {
+    "revision": "f5b53d193d2f73898b30bf73112c1f1c",
+    "url": "/index.html"
+  }
+];
