905eac4f8cf79fc91f3c4de12be44d10bb4e2588
fyeung21 <fyeung21@gmail.com>
2019-11-18 20:57:22 -0800

    start sketching shareItemform

--- client/src/components/ShareItemForm/ShareItemForm.js
+++ client/src/components/ShareItemForm/ShareItemForm.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-
+import { ItemPreviewContext } from '../../context/ItemProvider';
+import { withStyles } from '@material-ui/core/styles';
 class ShareForm extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-
+  onSubmit() {
+    console.log('test')
+  }
   render() {
+    const { classes } = this.props
+    console.log(classes)
     return (
-      <div>
-        <p>This is the share form.</p>
-      </div>
+      <ItemPreviewContext.Consumer>
+        {{ state, updatePreview, resetPreview }} => {
+          /* put Form in here 
+          
+          <div>
+          <p>This is the share form.</p>
+        </div>*/
+        }
+      </ItemPreviewContext.Consumer>
     );
   }
 }
-
 export default ShareForm;
