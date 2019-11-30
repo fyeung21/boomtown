82bfec77db2f54e9a33190162102bde6ed3c0d46
fyeung21 <fyeung21@gmail.com>
2019-11-30 12:27:13 -0800

    sketch profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import styles from './styles';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles";
+import SharePreviewCard from "../../components/ShareItemPreview/SharePreviewCard";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
 
 const Profile = ({ classes }) => {
   return (
@@ -8,6 +11,32 @@ const Profile = ({ classes }) => {
       <p>
         This is the profile page located at <code>/profile/:userId</code>.
       </p>
+      <Avatar
+        alt="Owner"
+        src="/static/images/avatar/1.jpg"
+        className={classes.bigAvatar}
+      />
+      <Typography
+        gutterBottom
+        variant="h3"
+        color="textSecondary"
+        component="h3"
+      >
+        Username
+      </Typography>
+      <Typography gutterBottom variant="body1" component="h3">
+        Items Shared
+      </Typography>
+      <Typography gutterBottom variant="body1" component="h3">
+        Items borrowed
+      </Typography>
+      <Typography variant="body1" component="h3">
+        "No bio provided"
+      </Typography>
+      <Typography gutterBottom variant="h4" color="primary" component="h3">
+        Shared Items
+      </Typography>
+      <SharePreviewCard />
     </div>
   );
 };
